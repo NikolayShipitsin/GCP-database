3e0874524c32406adc4800b7e9c462f36b838069
gtnick@mail.ru <gtnick@mail.ru>
2021-04-27 14:35:39 +0800

    indexses were update to SEQUENCES

--- tables/common.sql
+++ tables/common.sql
@@ -2,25 +2,28 @@ USE traindb;
 GO
 
 
-DROP TABLE IF EXISTS SoundFiles
+DROP TABLE IF EXISTS FileStates;
+DROP SEQUENCE IF EXISTS dbo.FileStatesIds;
 GO
 CREATE TABLE FileStates
 (
-    StateId TINYINT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    StateId TINYINT NOT NULL DEFAULT NEXT VALUE FOR dbo.PrototypesIds,
     StateName VARCHAR(250)
     CONSTRAINT [UX_State] UNIQUE (StateName)
 )
 GO
 
-DROP TABLE IF EXISTS SoundFiles
+DROP TABLE IF EXISTS SoundFiles;
+DROP SEQUENCE IF EXISTS dbo.SoundFilesIds;
 GO
 
 CREATE TABLE SoundFiles
 (
-    FileId INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    FileId INT NOT NULL  DEFAULT NEXT VALUE FOR dbo.PrototypesIds,
     StateId TINYINT NOT NULL,
     FileNameStr VARCHAR(250),
     DateOfAdd DATETIME
     CONSTRAINT [FK_SoundFiles_FileStates] FOREIGN KEY (StateId) REFERENCES FileStates (StateId),
     CONSTRAINT [UX_FileNameStr] UNIQUE (FileNameStr)
 )
+GO
