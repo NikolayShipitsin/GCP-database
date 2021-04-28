3264c2c3800db51ce22bc826c8613881930ff59d
gtnick@mail.ru <gtnick@mail.ru>
2021-04-28 14:13:09 +0800

    add_FileState procedure was added

--- procedures/FileStates/add_FileState.sql
+++ procedures/FileStates/add_FileState.sql
@@ -1,2 +1,28 @@
 DROP PROCEDURE IF EXISTS add_FileState;
 GO
+
+
+CREATE PROCEDURE add_FileState(
+	@StateId TINYINT,
+    @StateName varchar(250) 
+)
+AS
+BEGIN 
+
+    DECLARE
+        @resultCode INT = -1
+
+    INSERT INTO FileStates(
+        StateId,
+        StateName
+    )
+    VALUES(
+        @StateId,
+        @StateName
+    )
+
+    SET @resultCode = @@ERROR
+
+    RETURN @resultCode
+END;
+GO
