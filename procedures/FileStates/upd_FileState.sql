8b52495ad047f02a8531dfc35ea6b8a5d41b615f
Nikolay <gtnick@mail.ru>
2021-05-13 00:56:53 +0800

    File_State procedure was added but not finished

---
+++ procedures/FileStates/upd_FileState.sql
@@ -0,0 +1,23 @@
+DROP PROCEDURE IF EXISTS upd_Country;
+GO
+
+CREATE PROCEDURE upd_FileState(
+    @StateId TINYINT = NULL OUTPUT,
+    @StateName varchar(250) = NULL OUTPUT 
+)
+AS
+BEGIN
+    
+    DECLARE
+        -- Код возврата вложенных процедур, при наличии
+		@errorNumber int = 0,
+        -- Код возврата процедуры, по умолчанию 
+		@resultCode int = -1
+
+    DECLARE
+        @curStateId TINYINT = @StateId,
+        @curStateName varchar(250) = @StateName
+
+    EXEC @errorNumber = get_FileState
+        @curStateId,
+        @curStateName
